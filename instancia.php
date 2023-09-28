2f9a4b5350cb03d92e431d88a9ec95830b90b55a
YungAkina <105649833+YungAkina@users.noreply.github.com>
2023-09-28 09:43:47 -0300

    Update instancia.php

--- instancia.php
+++ instancia.php
@@ -1,20 +1,12 @@
 <?php
-require "aplicativo.php";
 require "conexao.php";
-require "repositorioaplicativo";
-$aplicativo = new aplicativo('spotify, musica, 400 milhoes, 4,4, Spotify AB');
+require "Aplicativo.php";
+require "RepositorioAplicativo.php";
 
-$repaplicativo= new repositorioaplicativo();
+$Aplicativo1 = new Aplicativo('Spotify', 'Música', '400 milhões', '4.4', 'Spotify AB');
 
-$aplicativo1 = new aplicativo('Spotify, Música, 400 milhões, 4,4 , Spotify AB');
-$aplicativo2 = new aplicativo('Whatsapp, mensagens, 2 bilhões, 4,5 , Whatsapp LLC');
-$aplicativo3 = new aplicativo('Netflix, filmes e séries, 10 milhões, 4,3 , DevOps');
+$repositorioAplicativo = new RepositorioAplicativo();
+$repositorioAplicativo->cadastrar($Aplicativo1, $banco);
 
-$repaplicativo->cadastrar($banco,$aplicativo1);
-$repaplicativo->cadastrar($banco,$aplicativo2);
-$repaplicativo->cadastrar($banco,$aplicativo3);
-
-$tabela = "CREATE TABLE IF NOT EXISTS aplicativo(nomeaplicativo TEXT, categoriaaplicativo TEXT, numerodedownloadsaplicativo TEXT, avaliacaoaplicativo TEXT, desenvolvedoraaplicativo TEXT, primary key(codigo))";
-
-$banco->query($tabela);
-
+echo "Aplicativo cadastrado com sucesso!";
+?>
