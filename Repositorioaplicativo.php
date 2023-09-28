3626068f0559012a96de9219e1a8e32c448640d5
YungAkina <105649833+YungAkina@users.noreply.github.com>
2023-09-28 09:42:45 -0300

    Update Repositorioaplicativo.php

--- Repositorioaplicativo.php
+++ Repositorioaplicativo.php
@@ -1,31 +1,28 @@
 <?php
-
-class Repositorioaplicativo{
-
-    public function cadastrar(PDO $banco, aplicativo $aplicativo){
-    $sqlInsert = "INSERT INTO Aplicativo(nome, categoria, numerodedownloads, avaliacao, desenvolvedora) VALUES(:n, :cat, :nd, :ava, :d)";
+class RepositorioAplicativo {
+    public function cadastrar(Aplicativo $aplicativo, PDO $pdo) {
+        $codigoSql = "INSERT INTO Aplicativo (nome, categoria, numerodownloads, avaliacao, desenvolvedora) VALUES (:n, :cat, :nd, :ava, :d)";
+        
+        $nome = $aplicativo->exibirNome();
+        $categoria = $aplicativo->exibirCategoria();
+        $numerodownloads = $aplicativo->exibirNumeroDownloads();
+        $avaliacao = $aplicativo->exibirAvaliacao();
+        $desenvolvedora = $aplicativo->exibirDesenvolvedora();
     
-    $insert = $banco->prepare($sqlInsert);
+        $insert = $pdo->prepare($codigoSql);
+        $insert->bindParam(":n", $nome);
+        $insert->bindParam(":cat", $categoria);
+        $insert->bindParam(":nd", $numerodownloads);
+        $insert->bindParam(":ava", $avaliacao);
+        $insert->bindParam(":d", $desenvolvedora);
+        
+        $insert->execute();
+    }
 
-    $nome = $aplicativo->exibirnome();
-    $categoria = $aplicativo->exibircategoria();
-    $numerodedownloads = $aplicativo->exibirnumerodedownloads();
-    $avaliacao = $aplicativo->exibiravaliacao();
-    $desenvolvedora = $aplicativo->exibirdesenvolvedora();
-
-    $insert->bindParam(":n", $nome);
-    $insert->bindParam(":cat" , $categoria);
-    $insert->bindParam(":nd" , $numerodedownloads);
-    $insert->bindParam(":ava" , $avaliacao);
-    $insert->bindParam(":d" , $desenvolvedora);
-
-    $insert->execute();
+    public function listar(PDO $pdo) {
+        $sql = "SELECT * FROM Aplicativo";
+        $select = $pdo->query($sql);
+        return $select->fetchAll(PDO::FETCH_ASSOC);
+    }
 }
-
-
-public function exibirtudo(PDO $banco){
-    $sql = "SELECT * FROM APLICATIVO";
-    $dado = $banco->query($sql);
-    return $dado->fetchAll(PDO::FETCH_ASSOC);
-}
-}
+?>
